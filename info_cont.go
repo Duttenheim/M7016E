4fd6454290df1efed8920ca2d0ff57fbf68e57cb
Polo417 <Polo417@users.noreply.github.com>
2014-02-18 23:21:44 +0100

    Update info_cont.go
    Clean version

--- info_cont.go
+++ info_cont.go
@@ -28,51 +28,40 @@ type CpuStat struct {
 	system int64
 }
 
+func read(path string) string {
+    bs, err := ioutil.ReadFile(path)
+    if err != nil {
+		fmt.Println("Error : requested file doesn't exist.")
+        return ""
+    }	
+    return (string(bs))
+}
 
-func main() {
-	
+func findId() string {	
 	var id,name [100]string								//Not dynamic !!
 	var indexId, indexName [200]int
 	var index int = 0									//index used to go through id and name
 	var keepFilling bool = true
-	
 	var containerName string = "polo417/docpal_old"		//name to change
-	var path string
-	
-	var memory_stat, memory_stat_total MemStat
-	var cpu_stat CpuStat
-	var memBuffer [13]int64
-	var cpuBuffer [2]int64
-	
-    /*--Reading the file called "ps"--*/
-    bs, err := ioutil.ReadFile("ps")
-    if err != nil {
-		fmt.Println("Error : requested file doesn't exist.")
-        return
-    }	
+    var idFound string
     
-    /*--retrieving all the characters of the file--*/
-    str := string(bs)
-    //fmt.Println(str)
-    
+    str := read("ps")
+ 
     /*--Displaying all the interesting characters of the file--*/
     for i := 0; i<len(strings.Split(str, " "))-1; i++ {
-		//fmt.Println(i)
 		
 		if ( (strings.Split(str, " ")[i]!="") && (strings.Split(str, " ")[i]!="About") && (strings.Split(str, " ")[i]!="CONTAINER") && (strings.Split(str, " ")[i]!="ID") && (strings.Split(str, " ")[i]!="IMAGE") && (strings.Split(str, " ")[i]!="COMMAND") && (strings.Split(str, " ")[i]!="CREATED") && (strings.Split(str, " ")[i]!="STATUS") && (strings.Split(str, " ")[i]!="PORTS") ){
 			if ( (strings.Split(strings.Split(str, " ")[i],"\n")[0]=="") || (i==136) ){
-				//fmt.Println("i=", i, " : ", strings.Split(strings.Split(str, " ")[i],"\n")[1]," +++ ", len(strings.Split(strings.Split(str, " ")[i],"\n")[1]))
 				indexId[index]=i
 				indexName[index]=i+3
 				index++
 			} else {
-				//fmt.Println("i=", i, " : ", strings.Split(str, " ")[i]," +++ ", len(strings.Split(str, " ")[i]))
 			}
 		}		
     }
     
     /*--retrieving the IDs and the names of the containers from the file--*/
-    index = 0
+    index=0
     for (keepFilling==true) {
 		if (indexId[index]!=0) {
 			id[index]=strings.Split(strings.Split(str, " ")[indexId[index]],"\n")[1]
@@ -82,81 +71,74 @@ func main() {
 			keepFilling=false
 		}
 	}
-    
-/*    fmt.Println("PIKAAAAAAAAAAAAAAAAA")
-	fmt.Println(indexId)
-	fmt.Println(indexName)
-	fmt.Println(id)
-	fmt.Println(name)
-	fmt.Println(keepFilling)	*/
 	
-	/*--Reading the right memory metric file--*/
 	index=0
 	for ( index < len(name) ) {
 		if ( (name[index]==containerName) || (name[index]==containerName+":latest") ) {
-			//fmt.Println("Found ! Index = ", index)
-			path="/sys/fs/cgroup/memory/lxc/"+id[index]+"/memory.stat"
+			idFound = id[index]
 			break
 		} else {
 			index++
 		}
 	}
-	//fmt.Println(containerName)
-	//fmt.Println(path)
-	bs, err = ioutil.ReadFile(path)
-    if err != nil {
-        return
-    }
-    str = string(bs)
-    fmt.Println(str)	
-	fmt.Println("PIKAAAAAAAAAAAAAAAAAAAAAA")
-	
-	/*--Assigning values to the variables used to store the memory metrics--*/
-	//fmt.Println(strings.Split(str,"\n"))
+	return idFound
+}
+
+func getMemInfo() MemStat {
+	var path,str string
+	var memory_stat MemStat
+	var memBuffer [13]int64
+
+	path="/sys/fs/cgroup/memory/lxc/"+findId()+"/memory.stat"
+	str = read(path)
 	for i:=0; i<13; i++ {
-		//fmt.Println(strings.Split(strings.Split(str,"\n")[i]," ")[1])
 		charizard, _ := strconv.ParseInt(strings.Split(strings.Split(str,"\n")[i]," ")[1], 0, 64)
-		//fmt.Println("CHAAAAAAAAAAAAAAAAAA", charizard)
 		memBuffer[i]=charizard;
 	}
 	memory_stat = MemStat {memBuffer[0],memBuffer[1],memBuffer[2],memBuffer[3],memBuffer[4],memBuffer[5],memBuffer[6],memBuffer[7],memBuffer[8],memBuffer[9],memBuffer[10],memBuffer[11],memBuffer[12]}
+	fmt.Println("Memory Statistics : ", memory_stat)
+	return memory_stat
+}
+
+func getMemTotInfo() MemStat {
+	var path,str string
+	var memory_stat_total MemStat
+	var memBuffer [13]int64
+
+	path="/sys/fs/cgroup/memory/lxc/"+findId()+"/memory.stat"
+	str = read(path)
 	for i:=0; i<13; i++ {
-		//fmt.Println(strings.Split(strings.Split(str,"\n")[i]," ")[1])
 		charizard, _ := strconv.ParseInt(strings.Split(strings.Split(str,"\n")[i]," ")[1], 0, 64)
-		//fmt.Println("CHAAAAAAAAAAAAAAAAAA", charizard)
 		memBuffer[i]=charizard;
 	}
 	memory_stat_total = MemStat {memBuffer[0],memBuffer[1],memBuffer[2],memBuffer[3],memBuffer[4],memBuffer[5],memBuffer[6],memBuffer[7],memBuffer[8],memBuffer[9],memBuffer[10],memBuffer[11],0}
-	
-	//fmt.Println(memBuffer)
-	fmt.Println("Memory Statistics : ", memory_stat)
 	fmt.Println("Memory Statistics (total) : ", memory_stat_total)
-	fmt.Println("\n")
+	return memory_stat_total
+}
+
+func getCpuInfo() CpuStat {
+	var path,str string
+	var cpu_stat CpuStat
+	var cpuBuffer [2]int64
 	
-	
-	/*--Reading the right CPU metric file--*/
-	//fmt.Println(index)
-	path="/sys/fs/cgroup/cpuacct/lxc/"+id[index]+"/cpuacct.stat"
-	//fmt.Println(path)
-	bs, err = ioutil.ReadFile(path)
-    if err != nil {
-        return
-    }
-    str = string(bs)
-    fmt.Println(str)
-    
+	path="/sys/fs/cgroup/cpuacct/lxc/"+findId()+"/cpuacct.stat"
+    str = read(path)
 	for i:=0; i<2; i++ {
-		//fmt.Println(strings.Split(strings.Split(str,"\n")[i]," ")[1])
 		charizard, _ := strconv.ParseInt(strings.Split(strings.Split(str,"\n")[i]," ")[1], 0, 64)
-		//fmt.Println("CHAAAAAAAAAAAAAAAAAA", charizard)
 		cpuBuffer[i]=charizard;
 	}
 	cpu_stat = CpuStat {cpuBuffer[0],cpuBuffer[1]}
-    
-	fmt.Println("PIKAAAAAAAAAAAAAAAAAAAAAA")	
-    
-    //fmt.Println(cpuBuffer)
     fmt.Println("CPU Statistics : ", cpu_stat)
-    
+    return cpu_stat
 }
+
+func main() {
+	getMemInfo()
+	getMemTotInfo()
+	getCpuInfo()
+}
+	
+
+
+
 
