26c7e8cf2918c82292fef4100ffee1bf7b843c81
Jon Martin Mikalsen <overmind_Zerg6@hotmail.com>
2014-05-13 16:12:45 +0200

    Added replycode enums that will be used for controlling reply from edgenodes

--- code/src/edgenode/dockerlistener.go
+++ code/src/edgenode/dockerlistener.go
@@ -31,6 +31,21 @@ type RemoveContainerArgs struct {
     Force bool
 }
 
+//Replycodes
+const (
+    ErrorCode		= 0
+    CreateContainer 	= 1
+    StartContainer      = 2
+    StopContainer     	= 3
+    KillContainer	= 4
+    RestartContainer    = 5
+    RemoveContainer	= 6
+    ListContainers      = 7
+    PullImage		= 8
+    RemoveImage		= 9
+    ListImages		= 10
+)
+
 type DockerListArgs struct {
 	ShowAll bool
 }
@@ -45,8 +60,11 @@ type RemoveImageArgs struct {
 	Name string
 }
 
+//Reply codes are used to define what method that it returns from
+// 
 type RpcOutput struct {
 	Content string
+	ReplyCode int
 }
 
 func (obj* EdgeNodeHandler) Test(input* ContainerArgs, output* RpcOutput) error {
@@ -62,9 +80,11 @@ func (obj* EdgeNodeHandler) CreateContainer(args* CreateContainerArgs, output* R
 	container, err := client.CreateContainer(createArgs)
 	output.Content = ""
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	}else{
 		output.Content += fmt.Sprintf("Container %s created successfully", container.ID)
+		output.ReplyCode = CreateContainer
 	}
 	return nil
 
@@ -77,9 +97,11 @@ func (obj* EdgeNodeHandler) StartContainer(args* ContainerArgs, output* RpcOutpu
 	output.Content = ""
     fmt.Printf("Container id:%s\n", args.ID)
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	}else{
 		output.Content += fmt.Sprintf("Container %s started", args.ID)
+		output.ReplyCode = StartContainer
 	}
 	return nil
 	
@@ -91,9 +113,11 @@ func (obj* EdgeNodeHandler) StopContainer(args* ContainerArgs, output* RpcOutput
 	err := client.StopContainer(args.ID, 3)
 	output.Content = ""
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	} else {
 		output.Content += fmt.Sprintf("Stopped container %s", args.ID)
+		output.ReplyCode = StopContainer
 	}
 	return nil
 }
@@ -104,9 +128,11 @@ func (obj* EdgeNodeHandler) KillContainer(args* ContainerArgs, output* RpcOutput
 	output.Content = ""
 	err := client.KillContainer(docker.KillContainerOptions{ID: args.ID})
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	}else{
 		output.Content += fmt.Sprintf("Container %s was killed", args.ID)
+		output.ReplyCode = KillContainer
 	}
 	return nil
 }
@@ -118,9 +144,11 @@ func (obj* EdgeNodeHandler) RestartContainer(args* ContainerArgs, output* RpcOut
 	output.Content = ""
 	err := client.RestartContainer(args.ID, 500)
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	}else{
 		output.Content += fmt.Sprintf("Container %s is restarting", args.ID)
+		output.ReplyCode = RestartContainer
 	}
 	return nil
 }
@@ -131,9 +159,11 @@ func (obj* EdgeNodeHandler) RemoveContainer(args* RemoveContainerArgs, output* R
 	output.Content = ""
 	err := client.RemoveContainer(docker.RemoveContainerOptions{ID: args.ID, RemoveVolumes: args.RemoveVolumes, Force: args.Force})
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	}else{
 		output.Content += fmt.Sprintf("Container %s was removed", args.ID)
+		output.ReplyCode = RemoveContainer
 	}
 	return nil
 }
@@ -147,8 +177,10 @@ func (obj* EdgeNodeHandler) ListContainers(args* DockerListArgs, output* RpcOutp
 	output.Content = ""
 	if err != nil {
 		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	} else {
 		output.Content += fmt.Sprintf("Containers found \n")
+		output.ReplyCode = ListContainers
 	}
 	for _, img := range imgs {
 		output.Content += fmt.Sprintf("ID: %s \n", img.ID)
@@ -163,9 +195,11 @@ func (obj* EdgeNodeHandler) PullImage(args* ImageArgs, output* RpcOutput) error 
 	output.Content = ""
 	err := client.PullImage(docker.PullImageOptions{Repository: args.Repository}, docker.AuthConfiguration{})
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	} else {
 		output.Content += fmt.Sprintf("Pulled container")
+		output.ReplyCode = PullImage
 	}
 	return nil
 }
@@ -176,9 +210,11 @@ func (obj* EdgeNodeHandler) RemoveImage(args* RemoveImageArgs, output* RpcOutput
 	output.Content = ""
 	err := client.RemoveImage(args.Name)
 	if err != nil {
-		output.Content += fmt.Sprintf("ERROR: %s", err)	
+		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	} else {
 		output.Content += fmt.Sprintf("Removed %s Image", args.Name)
+		output.ReplyCode = RemoveImage
 	}
 	return nil
 }
@@ -191,8 +227,10 @@ func (obj* EdgeNodeHandler) ListImages(args* DockerListArgs, output* RpcOutput) 
         imgs, err := client.ListImages(args.ShowAll)
 	if err != nil {
 		output.Content += fmt.Sprintf("ERROR: %s", err)
+		output.ReplyCode = ErrorCode
 	} else {
 		output.Content += fmt.Sprintf("Containers found \n")
+		output.ReplyCode = ListImages
 	}
         for _, img := range imgs {
                 output.Content += fmt.Sprintf("ID: ", img.ID)
