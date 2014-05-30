41624ea9faa7008131224ca8da36742ce3044109
AmelieA <amlaga-3@student.ltu.se>
2014-05-30 10:39:34 +0200

    improvement of pull images

--- TEST-ZONE/WEB/routes/servers/index.js
+++ TEST-ZONE/WEB/routes/servers/index.js
@@ -8,7 +8,7 @@ exports.index = function(req, res)
 		  				server2: '130.240.134.117:2020', 
 		  				server3: '130.240.134.118:2020', 
 		  				server4: '130.240.134.119', 
-		  				server5: '127.0.0.1:2020',
+		  				server5: '192.168.92.30:2020',
 		  				server6: '130.240.134.120:2020'};
   res.render('servers', { title: 'Servers',  servers: serverList })
   
