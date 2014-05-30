fda6eeca13b095f9a0ad945ebd319f0c682daaaf
Jon Martin Mikalsen <overmind_Zerg6@hotmail.com>
2014-05-30 10:07:17 +0200

    Updated some text around on the pages, and added 2 more edge-nodes to upload script

--- TEST-ZONE/WEB/routes/servers/index.js
+++ TEST-ZONE/WEB/routes/servers/index.js
@@ -4,12 +4,9 @@
 
 exports.index = function(req, res)
 {
-  var serverList = { server1: '130.240.134.116', 
-		  				server2: '130.240.134.117:2020', 
-		  				server3: '130.240.134.118:2020', 
-		  				server4: '130.240.134.119', 
-		  				server5: '127.0.0.1:2020',
-		  				server6: '130.240.134.120:2020'};
-  res.render('servers', { title: 'Servers',  servers: serverList })
+  var serverList = { server1: '130.240.134.117:2020', 
+		  				server2: '130.240.134.120:2020', 
+		  				server5: '127.0.0.1:2020'};
+  res.render('servers', { title: 'SuperNode',  servers: serverList })
   
 };
